ee2cb4ac98b4817fa76da3ab6d28d014f76e5bc0
19048147 <19048147@BERNADETTE-ASUS>
2020-08-19 00:25:49 +0800

    updated main class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -6,42 +6,62 @@ public class C206_CaseStudy {
 
 		// Bernadette
 		ArrayList<MenuItem> menuItemList = new ArrayList<MenuItem>();
+		
 		// Ariezal
 		ArrayList<Account> accountList = new ArrayList<Account>();
+		
 		// Kenneth
 		ArrayList<Order> orderList = new ArrayList<Order>();
+		
 		// Karthik
 		ArrayList<PurchaseOrder> purchaseOrderList = new ArrayList<PurchaseOrder>();
-		
+
 		int option = 0;
-		
+
 		while (option != 5) {
-			
+
 			C206_CaseStudy.optionMenu();
 			option = Helper.readInt("Enter an option > ");
-			
+
 			if (option == 1) {
 				// Bernadette
-				
-				
+				// View Menu Items
+				C206_CaseStudy.setHeader("View, Add, Delete Menu Items");
+				userTypeMenu();
+
+				int itemType = Helper.readInt("Enter option to select item type > ");
+
+				if (itemType == 1) {
+					// View All Menu Item
+					C206_CaseStudy.viewAllMenuItem(menuItemList);
+
+				} else if (itemType == 2) {
+					// Add Menu Item
+					C206_CaseStudy.addMenuItem(menuItemList);
+
+				} else if (itemType == 3) {
+					// Delete Menu Item
+					C206_CaseStudy.deleteMenuItem(menuItemList);
+
+				} else {
+					System.out.println("Invalid type");
+				}
+
 			} else if (option == 2) {
 				// Ariezal
-				
-				
+
 			} else if (option == 3) {
 				// Kenneth
-				
-				
+
 			} else if (option == 4) {
 				// Karthik
-				
-				
+
 			} else if (option == 5) {
-				
+
 				System.out.println("Thank you for using this canteen app");
 			} else {
 				System.out.println("Invalid option");
-				
+
 			}
 		}
 
@@ -75,6 +95,13 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");
 	}
 
+	// ================================= Option 1 Add, View, Delete Menu Item =================================
+
+	// Bernadette
+	public static void viewAllMenuItem(ArrayList<MenuItem> menuItemList) {
+
+	}
+
 	// Bernadette
 	public static void addMenuItem(ArrayList<MenuItem> menuItemList) {
 
@@ -85,9 +112,10 @@ public class C206_CaseStudy {
 
 	}
 
-	// Bernadette
-	public static void viewAllMenuItem(ArrayList<MenuItem> menuItemList) {
+	// ================================= Option 2 Add, View, Delete Account =================================
 
-	}
+	// ================================= Option 3 Add, View, Delete Order ===================================
+
+	// ================================= Option 4 Add, View, Purchase Order =================================
 
 }
