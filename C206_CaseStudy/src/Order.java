ff918be92243d934183e902f2e7924a838e99193
19048147 <19048147@BERNADETTE-ASUS>
2020-08-19 17:14:25 +0800

    updated Order Class for Kenneth's task because he have forgotten

--- C206_CaseStudy/src/Order.java
+++ C206_CaseStudy/src/Order.java
@@ -1,5 +1,56 @@
+import java.util.ArrayList;
 
 // Kenneth
 public class Order {
+	
+	private String username;
+	private String status;
+	private boolean takeaway;
+	private ArrayList<MenuItem> items;
+	
+	public Order(String username, String status, boolean takeaway, ArrayList<MenuItem> items) {
+		this.username = username;
+		this.status = status;
+		this.takeaway = takeaway;
+		this.items = items;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public boolean isTakeaway() {
+		return takeaway;
+	}
+
+	public void setTakeaway(boolean takeaway) {
+		this.takeaway = takeaway;
+	}
+
+	public ArrayList<MenuItem> getItems() {
+		return items;
+	}
+
+	public void setItems(ArrayList<MenuItem> items) {
+		this.items = items;
+	}
+	
+	public String toString() {
+		return null;
+	}
+	
+	
 
 }
