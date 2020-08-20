9edc8a140a0a882ebd524d52b256dbd541dbebe5
19048147 <19048147@BERNADETTE-ASUS>
2020-08-20 22:49:07 +0800

    updated class

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -9,13 +9,12 @@ public class C206_CaseStudy {
 		menuItemList.add(new MenuItem("Smoothies", "Strawberry", 4.70));
 		menuItemList.add(new MenuItem("Soda", "Coca Cola", 1.50));
 
-		// Ariezal 
+		// Ariezal
 		ArrayList<Account> accountList = new ArrayList<Account>();
-		accountList.add(new Account("James", "canteen123" , "Staff" ));
+		accountList.add(new Account("James", "canteen123", "Staff"));
 		accountList.add(new Account("Zac", "canteen123", "Canteen Admin"));
 		accountList.add(new Account("Joe", "canteen123", "Customer"));
-		accountList.add(new Account("Aaron", "canteen123" , "Staff" ));
-		
+		accountList.add(new Account("Aaron", "canteen123", "Staff"));
 
 		// Kenneth
 		ArrayList<Order> orderList = new ArrayList<Order>();
@@ -59,9 +58,8 @@ public class C206_CaseStudy {
 				// Ariezal
 				C206_CaseStudy.setHeader("View, Add, Delete Account");
 				userTypeMenu();
-				
+
 				int itemType = Helper.readInt("Enter option to select type > ");
-				
 
 			} else if (option == 3) {
 				// Kenneth
@@ -90,7 +88,7 @@ public class C206_CaseStudy {
 				// Karthik
 				C206_CaseStudy.setHeader("View, Add, Delete Purchase Order");
 				userTypeMenu();
-				
+
 				int itemType = Helper.readInt("Enter option to select type > ");
 
 			} else if (option == 5) {
@@ -129,21 +127,22 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");
 	}
 
-	// ================================= (Bernadette) Option 1 Add, View, Delete Menu Item =================================
+	// ================================= (Bernadette) Option 1 Add, View, Delete
+	// Menu Item =================================
 
 	public static String retrieveAllMenuItems(ArrayList<MenuItem> menuItemList) {
 		String output = "";
 		for (int i = 0; i < menuItemList.size(); i++) {
 
-			output += String.format("%-10s %-30s %-10s\n", 
-					menuItemList.get(i).getCategory(), menuItemList.get(i).getName(), menuItemList.get(i).getPrice());
+			output += String.format("%-10s %-30s %-10s\n", menuItemList.get(i).getCategory(),
+					menuItemList.get(i).getName(), menuItemList.get(i).getPrice());
 
 			output += String.format("%-10s %-30s %-10.2f\n", menuItemList.get(i).getCategory(),
 					menuItemList.get(i).getName(), menuItemList.get(i).getPrice());
 		}
 		return output;
 	}
-	
+
 	public static void viewAllMenuItem(ArrayList<MenuItem> menuItemList) {
 		for (int i = 0; i < menuItemList.size(); i++) {
 			C206_CaseStudy.setHeader("MENU ITEM LIST");
@@ -153,26 +152,26 @@ public class C206_CaseStudy {
 		}
 
 	}
-	
+
 	public static MenuItem inputMenuItem() {
 		String category = Helper.readString("Enter category > ");
 		String name = Helper.readString("Enter name > ");
 		double price = Helper.readDouble("Enter price > ");
-		
+
 		MenuItem m = new MenuItem(category, name, price);
 		return m;
 	}
 
 	public static void addMenuItem(ArrayList<MenuItem> menuItemList, MenuItem m) {
-		
+
 		menuItemList.add(m);
 		System.out.println("Menu Item Added");
 
 	}
-	
+
 	public static boolean doDelete(ArrayList<MenuItem> menuItemList, String category, String name, double price) {
 		boolean isDeleted = false;
-		
+
 		for (int i = 0; i < menuItemList.size(); i++) {
 			String c = menuItemList.get(i).getCategory();
 			if (category.equalsIgnoreCase(c)) {
@@ -183,7 +182,6 @@ public class C206_CaseStudy {
 		return isDeleted;
 	}
 
-	
 	public static void deleteMenuItem(ArrayList<MenuItem> menuItemList) {
 		C206_CaseStudy.viewAllMenuItem(menuItemList);
 		String category = Helper.readString("Enter category > ");
@@ -195,11 +193,14 @@ public class C206_CaseStudy {
 		} else {
 			System.out.println("Menu Item " + category + " is deleted");
 		}
-		
 
 	}
 
-	// ================================= Option 2 Add, View, Delete Account =================================
+	// ================================= Option 2 Add, View, Delete Account
+	// =================================
+
+	// ================================= Option 3 Add, View, Delete Order
+	// ===================================
 
 	// Kenneth
 	public static void viewAllOrder(ArrayList<Order> orderList) {
@@ -216,8 +217,7 @@ public class C206_CaseStudy {
 
 	}
 
-	// ================================= Option 3 Add, View, Delete Order ===================================
-
-	// ================================= Option 4 Add, View, Purchase Order =================================
+	// ================================= Option 4 Add, View, Purchase Order
+	// =================================
 
 }
